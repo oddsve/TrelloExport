bfda4cc1fb9bd026748f7a00664697e3167ef93d
Odd Morten Sveås <odd.morten@sveas.org>
2015-01-30 09:31:03 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,40 @@
+var onAutorize = function() {
+  Trello.rest("GET","boards/532169c1348424717a6298f5/lists/", function(lister){
+
+    $.each(lister, function(indx, liste){
+
+
+      $("#lister").append("<h2>"+ liste.name +"</h2><div id='" + liste.id + "'></div>");
+
+      Trello.rest("GET","lists/" + liste.id + "/cards", function(cards){
+
+        $.each( cards, function(indx, card){
+          $("#"+liste.id).append("<h3>" + card.name + "</h3>");
+          if (card.due != null ) {
+            $("#"+liste.id).append("<p class='frist'>Frist: <span class='dato'> " + moment(card.due, "YYYY-MM-DDTHH:mm:ssZ").format("D. MMM YYYY") + "</span></p>");
+          }
+          $("#"+liste.id).append("<div id=" + card.id + " class='beskrivelse'></div>");
+          $("#"+card.id ).append(marked(card.desc) );
+          $.each( card.labels , function (indx, label){
+            $("#"+liste.id).append("<p class='trello-label " + label.color + "'>" + label.name + "</p>")
+          });
+
+
+        });
+      });
+    });
+  });
+};
+
+
+var onError = function(){
+  alert("Error");
+};
+
+$("#get-board").click(function(){
+  Trello.authorize({name: "TrelloExport", type: "popup", success: onAutorize, error: onError, account:true  })
+});
+
+$("#logout").click(function(){
+  Trello.deauthorize();
+});
