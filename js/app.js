ebda07a86bd31db8b5a91cfabc767735c62764bf
Odd Morten Sveås <odd.morten@sveas.org>
2015-02-12 11:11:04 +0100

    Skuling av tomme lister

--- js/app.js
+++ js/app.js
@@ -4,11 +4,13 @@ var onAutorize = function() {
     $.each(lister, function(indx, liste){
 
 
-      $("#lister").append("<h2>"+ liste.name +"</h2><div id='" + liste.id + "'></div>");
+      $("#lister").append("<h2 id='h"+ liste.id +"'>"+ liste.name +"</h2><div id='" + liste.id + "'></div>");
+      $("#h" + liste.id).addClass("hidden");
 
       Trello.rest("GET","lists/" + liste.id + "/cards", function(cards){
 
         $.each( cards, function(indx, card){
+          $("#h" + liste.id).removeClass("hidden");
           $("#"+liste.id).append("<h3>" + card.name + "</h3>");
           if (card.due != null ) {
             $("#"+liste.id).append("<p class='frist'>Frist: <span class='dato'> " + moment(card.due, "YYYY-MM-DDTHH:mm:ssZ").format("D. MMM YYYY") + "</span></p>");
